43ce4b94cf8faff6adb61b526390f19e926861b8
spatnjak <spatnjak@fel.cvut.cz>
2014-10-26 21:11:54 +0100

    Basic Maven structure, Dependepcies, Build settings, Database settings

---
+++ src/main/java/cz/kubaspatny/opendayapp/service/AuthService.java
@@ -0,0 +1,71 @@
+package cz.kubaspatny.opendayapp.service;
+
+import cz.kubaspatny.opendayapp.dao.GenericDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.cache.NullUserCache;
+import org.springframework.transaction.support.TransactionTemplate;
+
+/**
+ * Author: Kuba Spatny
+ * Web: kubaspatny.cz
+ * E-mail: kuba.spatny@gmail.com
+ * Date: 26/10/2014
+ * Time: 18:35
+ * Copyright 2014 Jakub Spatny
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class AuthService extends AbstractUserDetailsAuthenticationProvider {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractUserDetailsAuthenticationProvider.class);
+    private GenericDao genericDAO;
+    private TransactionTemplate transactionTemplate;
+
+    public AuthService() {
+        this.setUserCache(new NullUserCache());
+    }
+
+    @Override
+    protected void additionalAuthenticationChecks(UserDetails ud, UsernamePasswordAuthenticationToken upat) throws AuthenticationException {
+        // do nothing
+    }
+
+    /**
+     * @param username
+     * @param upat
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    protected UserDetails retrieveUser(final String username, final UsernamePasswordAuthenticationToken upat) throws AuthenticationException {
+
+        //TODO: Add user retrieval
+
+        return null;
+    }
+
+    public void setGenericDAO(GenericDao genericDAO) {
+        this.genericDAO = genericDAO;
+    }
+
+    public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
+        this.transactionTemplate = transactionTemplate;
+    }
+
+}
