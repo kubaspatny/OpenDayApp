880f03d42405b029c9df64853e351c7d1a1c9205
spatnjak <spatnjak@fel.cvut.cz>
2015-01-02 16:13:35 +0100

    Finished SecuredRouteServiceTest, SecuredStationServiceTest

--- src/test/java/cz/kubaspatny/opendayapp/test/secured/SecuredStationServiceTest.java
+++ src/test/java/cz/kubaspatny/opendayapp/test/secured/SecuredStationServiceTest.java
@@ -1,16 +1,15 @@
 package cz.kubaspatny.opendayapp.test.secured;
 
-import cz.kubaspatny.opendayapp.bo.Event;
-import cz.kubaspatny.opendayapp.bo.Route;
-import cz.kubaspatny.opendayapp.bo.Station;
 import cz.kubaspatny.opendayapp.bo.User;
 import cz.kubaspatny.opendayapp.dao.GenericDao;
 import cz.kubaspatny.opendayapp.dto.EventDto;
 import cz.kubaspatny.opendayapp.dto.RouteDto;
 import cz.kubaspatny.opendayapp.dto.StationDto;
 import cz.kubaspatny.opendayapp.dto.UserDto;
+import cz.kubaspatny.opendayapp.exception.DataAccessException;
 import cz.kubaspatny.opendayapp.service.IEventService;
 import cz.kubaspatny.opendayapp.service.IRouteService;
+import cz.kubaspatny.opendayapp.service.IStationService;
 import cz.kubaspatny.opendayapp.service.IUserService;
 import cz.kubaspatny.opendayapp.test.AbstractSecuredTest;
 import org.joda.time.DateTime;
@@ -33,9 +32,9 @@ import java.util.List;
  * Author: Kuba Spatny
  * Web: kubaspatny.cz
  * E-mail: kuba.spatny@gmail.com
- * Date: 30/12/2014
- * Time: 22:05
- * Copyright 2014 Jakub Spatny
+ * Date: 2/1/2015
+ * Time: 15:34
+ * Copyright 2015 Jakub Spatny
  * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -49,12 +48,14 @@ import java.util.List;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-public class SecuredRouteServiceTest extends AbstractSecuredTest {
+public class SecuredStationServiceTest extends AbstractSecuredTest {
 
-    @Autowired private GenericDao dao;
+    @Autowired
+    private GenericDao dao;
     @Autowired private IUserService userService;
     @Autowired private IEventService eventService;
     @Autowired private IRouteService routeService;
+    @Autowired private IStationService stationService;
 
     private String username = "kuba.spatny@gmail.com";
 
@@ -64,6 +65,7 @@ public class SecuredRouteServiceTest extends AbstractSecuredTest {
     private String stationManager1 = "stationManager1@gmail.com";
     private String stationManager2 = "stationManager2@gmail.com";
 
+    private Long stationId;
     private Long routeId;
 
     @Before
@@ -102,13 +104,7 @@ public class SecuredRouteServiceTest extends AbstractSecuredTest {
 
         userService.createGeneratedUser(stationManager1);
         userService.createGeneratedUser(stationManager2);
-    }
 
-    @Test
-    public void testAddAndGetRoute() throws Exception {
-
-        UserDto u = userService.getUser(username);
-        System.out.println(u.getEvents().get(1));
         Long eventId = u.getEvents().get(1).getId();
 
         String name = "CREATED_ROUTE";
@@ -143,10 +139,18 @@ public class SecuredRouteServiceTest extends AbstractSecuredTest {
         Assert.assertEquals(times.size(), routeIds.size());
         Assert.assertEquals(1, routeIds.size());
 
-        Long id = routeIds.get(0);
+        routeId = routeIds.get(0);
+        stationId = routeService.getRoute(routeId).getStations().get(0).getId();
+
+    }
+
+    @Test
+    public void testGetStation() throws Exception {
 
         try {
-            routeService.getRoute(id);
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, "password", null);
+            SecurityContextHolder.getContext().setAuthentication(token);
+            stationService.getStation(stationId);
         } catch (AccessDeniedException e){
             Assert.fail("Should NOT have thrown exception!");
         }
@@ -154,7 +158,7 @@ public class SecuredRouteServiceTest extends AbstractSecuredTest {
         try {
             UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken("guide1", "password", null);
             SecurityContextHolder.getContext().setAuthentication(token);
-            routeService.getRoute(id);
+            stationService.getStation(stationId);
         } catch (AccessDeniedException e){
             Assert.fail("Should NOT have thrown exception!");
         }
@@ -162,7 +166,7 @@ public class SecuredRouteServiceTest extends AbstractSecuredTest {
         try {
             UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken("unknown", "password", null);
             SecurityContextHolder.getContext().setAuthentication(token);
-            routeService.getRoute(id);
+            stationService.getStation(stationId);
             Assert.fail("Should have thrown an exception!");
         } catch (AccessDeniedException e){
             // correct
@@ -171,50 +175,21 @@ public class SecuredRouteServiceTest extends AbstractSecuredTest {
     }
 
     @Test
-    public void testAddAndRemoveRoute() throws Exception {
+    public void testAddStation() throws Exception {
 
-        UserDto u = userService.getUser(username);
-        System.out.println(u.getEvents().get(1));
-        Long eventId = u.getEvents().get(1).getId();
+        StationDto s = new StationDto();
+        s.setName("Added station");
+        s.setSequencePosition(10);
+        s.setLocation("K-00-NEW");
+        s.setRelocationTime(100);
+        s.setTimeLimit(100);
 
-        String name = "CREATED_ROUTE";
-        String hexColor = "006080";
-        String info = "This is route information.";
-
-        List<DateTime> times = new ArrayList<DateTime>();
-        times.add(DateTime.now().plusHours(1));
-
-        HashMap<Integer, String> guides = new HashMap<Integer, String>();
-
-        List<StationDto> stations = new ArrayList<StationDto>();
-        for (int i = 1; i <= 2; i++) {
-
-            StationDto s = new StationDto(true);
-            s.setName("Station " + i);
-            s.setSequencePosition(i);
-            s.setLocation("K-00" + i);
-            s.setRelocationTime(i*10);
-            s.setTimeLimit(i*100);
-
-            guides.put(i, "guide"+ i +"@gmail.com");
-            stations.add(s);
-
-        }
-
-        List<String> stationManagerEmails = new ArrayList<String>();
-        stationManagerEmails.add(stationManager1);
-        stationManagerEmails.add(stationManager2);
-
-        List<Long> routeIds = routeService.saveRoute(eventId, name, hexColor, info, times, stations, guides, stationManagerEmails);
-        Assert.assertEquals(times.size(), routeIds.size());
-        Assert.assertEquals(1, routeIds.size());
-
-        Long id = routeIds.get(0);
+        Long newStationId = null;
 
         try {
             UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken("guide1", "password", null);
             SecurityContextHolder.getContext().setAuthentication(token);
-            routeService.removeRoute(id);
+            newStationId = stationService.addStation(routeId, s);
             Assert.fail("Should have thrown an exception!");
         } catch (AccessDeniedException e){
             // correct
@@ -223,7 +198,7 @@ public class SecuredRouteServiceTest extends AbstractSecuredTest {
         try {
             UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken("unknown", "password", null);
             SecurityContextHolder.getContext().setAuthentication(token);
-            routeService.removeRoute(id);
+            newStationId = stationService.addStation(routeId, s);
             Assert.fail("Should have thrown an exception!");
         } catch (AccessDeniedException e){
             // correct
@@ -232,60 +207,54 @@ public class SecuredRouteServiceTest extends AbstractSecuredTest {
         try {
             UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, "password", null);
             SecurityContextHolder.getContext().setAuthentication(token);
-            routeService.removeRoute(id);
+            newStationId = stationService.addStation(routeId, s);
+            Assert.assertNotNull(newStationId);
         } catch (AccessDeniedException e){
             Assert.fail("Should NOT have thrown exception!");
+        }
+
+        try {
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, "password", null);
+            SecurityContextHolder.getContext().setAuthentication(token);
+            stationService.getStation(newStationId);
+        } catch (AccessDeniedException e){
+            Assert.fail("Should NOT have thrown exception!");
+        }
+
+        try {
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken("guide1", "password", null);
+            SecurityContextHolder.getContext().setAuthentication(token);
+            stationService.getStation(newStationId);
+        } catch (AccessDeniedException e){
+            Assert.fail("Should NOT have thrown exception!");
+        }
+
+        try {
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken("unknown", "password", null);
+            SecurityContextHolder.getContext().setAuthentication(token);
+            stationService.getStation(newStationId);
+            Assert.fail("Should have thrown an exception!");
+        } catch (AccessDeniedException e){
+            // correct
         }
 
     }
 
     @Test
-    public void testAddAndUpdateRoute() throws Exception {
+    public void testUdpdateStation() throws Exception {
 
-        UserDto u = userService.getUser(username);
-        System.out.println(u.getEvents().get(1));
-        Long eventId = u.getEvents().get(1).getId();
 
-        String name = "CREATED_ROUTE";
-        String hexColor = "006080";
-        String info = "This is route information.";
-
-        List<DateTime> times = new ArrayList<DateTime>();
-        times.add(DateTime.now().plusHours(1));
-
-        HashMap<Integer, String> guides = new HashMap<Integer, String>();
-
-        List<StationDto> stations = new ArrayList<StationDto>();
-        for (int i = 1; i <= 2; i++) {
-
-            StationDto s = new StationDto(true);
-            s.setName("Station " + i);
-            s.setSequencePosition(i);
-            s.setLocation("K-00" + i);
-            s.setRelocationTime(i*10);
-            s.setTimeLimit(i*100);
-
-            guides.put(i, "guide"+ i +"@gmail.com");
-            stations.add(s);
-
-        }
-
-        List<String> stationManagerEmails = new ArrayList<String>();
-        stationManagerEmails.add(stationManager1);
-        stationManagerEmails.add(stationManager2);
-
-        List<Long> routeIds = routeService.saveRoute(eventId, name, hexColor, info, times, stations, guides, stationManagerEmails);
-        Assert.assertEquals(times.size(), routeIds.size());
-        Assert.assertEquals(1, routeIds.size());
-
-        Long id = routeIds.get(0);
-        RouteDto routeDto = routeService.getRoute(id);
-        routeDto.setInformation("NEW INFO!");
+        StationDto s = stationService.getStation(stationId);
+        s.setName("Updated station");
+        s.setSequencePosition(10);
+        s.setLocation("K-00-NEW");
+        s.setRelocationTime(100);
+        s.setTimeLimit(100);
 
         try {
             UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken("guide1", "password", null);
             SecurityContextHolder.getContext().setAuthentication(token);
-            routeService.updateRoute(routeDto);
+            stationService.updateStation(s);
             Assert.fail("Should have thrown an exception!");
         } catch (AccessDeniedException e){
             // correct
@@ -294,7 +263,7 @@ public class SecuredRouteServiceTest extends AbstractSecuredTest {
         try {
             UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken("unknown", "password", null);
             SecurityContextHolder.getContext().setAuthentication(token);
-            routeService.updateRoute(routeDto);
+            stationService.updateStation(s);
             Assert.fail("Should have thrown an exception!");
         } catch (AccessDeniedException e){
             // correct
@@ -303,9 +272,46 @@ public class SecuredRouteServiceTest extends AbstractSecuredTest {
         try {
             UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, "password", null);
             SecurityContextHolder.getContext().setAuthentication(token);
-            routeService.updateRoute(routeDto);
+            stationService.updateStation(s);
         } catch (AccessDeniedException e){
             Assert.fail("Should NOT have thrown exception!");
+        }
+
+    }
+
+    @Test
+    public void testRemoveStation() throws Exception {
+
+        try {
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken("guide1", "password", null);
+            SecurityContextHolder.getContext().setAuthentication(token);
+            stationService.removeStation(stationId);
+            Assert.fail("Should have thrown an exception!");
+        } catch (AccessDeniedException e){
+            // correct
+        }
+
+        try {
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken("unknown", "password", null);
+            SecurityContextHolder.getContext().setAuthentication(token);
+            stationService.removeStation(stationId);
+            Assert.fail("Should have thrown an exception!");
+        } catch (AccessDeniedException e){
+            // correct
+        }
+
+        try {
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, "password", null);
+            SecurityContextHolder.getContext().setAuthentication(token);
+            Assert.assertNotNull(stationService.getStation(stationId));
+            stationService.removeStation(stationId);
+
+            stationService.getStation(stationId);
+            Assert.fail("Should have thrown an Exception!");
+        } catch (AccessDeniedException e){
+            Assert.fail("Should NOT have thrown AccessDeniedException exception!");
+        } catch (DataAccessException e){
+            Assert.assertEquals(DataAccessException.ErrorCode.INSTANCE_NOT_FOUND, e.getErrorCode());
         }
 
     }
